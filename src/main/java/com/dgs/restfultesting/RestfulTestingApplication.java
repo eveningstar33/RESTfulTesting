16ad200d25be6c1443620390cda9044df3e17d67
Dragos Sarpe <contact@usdgadget.com>
2018-10-05 12:55:24 +0300

    Initial import of Java project

---
+++ src/main/java/com/dgs/restfultesting/RestfulTestingApplication.java
@@ -0,0 +1,12 @@
+package com.dgs.restfultesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulTestingApplication.class, args);
+	}
+}
