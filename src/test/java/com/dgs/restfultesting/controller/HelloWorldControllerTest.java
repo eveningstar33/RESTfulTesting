f4e5fb81753f6f137b31a3fb9c709b6c56341197
Dragos Sarpe <contact@usdgadget.com>
2018-10-05 14:54:54 +0300

    Improved the unit test to use matchers to check the status and content

--- src/test/java/com/dgs/restfultesting/controller/HelloWorldControllerTest.java
+++ src/test/java/com/dgs/restfultesting/controller/HelloWorldControllerTest.java
@@ -1,6 +1,8 @@
 package com.dgs.restfultesting.controller;
 
 import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,14 +15,14 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-// This test is a Spring test and I only want to test HelloWorldController
+// I only want to load and test HelloWorldController. Spring Mock Mvc framework help us to do that 
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(HelloWorldController.class)
+@RunWith(SpringRunner.class)              // We launched up a Spring context
+@WebMvcTest(HelloWorldController.class)   // We only launched up the HelloWorldController
 public class HelloWorldControllerTest {
 	
 	@Autowired
-	private MockMvc mockMvc;
+	private MockMvc mockMvc;                       // We use the MockMvc framework
 
 	@Test
 	public void helloWorld_basic() throws Exception {
@@ -31,10 +33,13 @@ public class HelloWorldControllerTest {
 				.get("/hello-world") 
 				.accept(MediaType.APPLICATION_JSON); 
 		
-		MvcResult result = mockMvc.perform(request).andReturn();  
+		MvcResult result = mockMvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().string("Hello World"))
+				.andReturn();  
 		
 		// We need to verify if the response contains "Hello World"
 		
-		assertEquals("Hello World", result.getResponse().getContentAsString()); 
+		// assertEquals("Hello World", result.getResponse().getContentAsString()); 
 	}
 }
